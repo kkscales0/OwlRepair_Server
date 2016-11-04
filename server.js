39ec382b4bb3bdfafa90c3d33b6d06d539e21555
Kyle Scales <kkscales0@gmail.com>
2016-11-04 18:22:40 -0400

    Create package.json and server.js
    package.json contains the dependencies needed to run server.js
    server.js starts the web service, connects to the mysql database and routes API calls from client

---
+++ server.js
@@ -0,0 +1,82 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var mysql 	   = require("mysql");
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+var db_con = mysql.createConnection({	// connection to mysql database
+    waitForConnections: true,
+	connectionLimit : 20,
+    host: 'localhost', //"104.196.219.23",
+    user: "root",
+    password: "",
+    database: "owl_repair",//_dev",
+    multipleStatements: true
+});
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// on routes that end in /campus
+// ---------------------------------------------------
+
+router.route('/campus')
+
+	// create a campus using POST
+	.post(function(req, res) {
+	
+		db_con.query('SET @campus_id = 0; CALL sp_create_campus(@campus_id, ?); SELECT @campus_id', [req.body.name], function(err,rows){
+			if (err) throw err;
+
+			var newId = rows[2][0]["@campus_id"];
+			res.json({ newId });
+		});
+	});
+
+router.route('/categories')
+
+	.get(function(req, res) {
+        
+        db_con.query('CALL sp_get_categories()',function(err,rows){
+			if (err) throw err;
+
+			res.json({ categories: rows[0] });
+		});
+    });
+	
+	
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
