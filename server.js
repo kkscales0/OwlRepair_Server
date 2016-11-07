27dfeee09534baae09a1f3e9eb95710b9bf537f3
Kyle Scales <kkscales0@gmail.com>
2016-11-06 23:01:09 -0500

    Add file required by Google Cloud.

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;        // set our port
 
 var db_con = mysql.createConnection({	// connection to mysql database
-    waitForConnections: true,
 	connectionLimit : 20,
-    host: 'localhost', //"104.196.219.23",
+    host: "104.196.219.23",
     user: "root",
-    password: "",
-    database: "owl_repair",//_dev",
+    password: "root",
+    database: "owl_repair_dev",
     multipleStatements: true
 });
 
@@ -46,19 +45,39 @@ router.route('/campus')
 	.post(function(req, res) {
 	
 		db_con.query('SET @campus_id = 0; CALL sp_create_campus(@campus_id, ?); SELECT @campus_id', [req.body.name], function(err,rows){
-			if (err) throw err;
+			if (err) {
+                console.log(err);
+                throw err;
+            }
 
 			var NEWID = rows[2][0]["@campus_id"];
 			res.json({ NEWID });
 		});
 	});
 
+router.route('/test')
+
+	.get(function(req, res) {
+	
+        connection.query('SELECT * FROM LU_CATEGORY', function(err, result) {
+            
+          if (err) throw err;
+
+          console.log(result.insertId);
+        });
+	});
+
+
+
 router.route('/categories')
 
 	.get(function(req, res) {
         
         db_con.query('CALL sp_get_categories()', function(err,rows){
-			if (err) throw err;
+			if (err) {
+                console.log(err);
+                throw err;
+            }
 
 			res.json({ CATEGORIES: rows[0] });
 		});
@@ -70,7 +89,10 @@ router.route('/request/submit')
 
         db_con.query('SET @request_id = 0; CALL sp_create_request(@request_id, ?, ?, ?, ?, ?, ?, ?, ?); SELECT @request_id', 
                      [req.body.userId, req.body.campusId, req.body.buildingId, req.body.locationDesc, req.body.categoryId ,req.body.desc, req.body.imagePath, req.body.public], function(err,rows){
-			if (err) throw err;
+			if (err) {
+                console.log(err);
+                throw err;
+            }
 
 			var NEWID = rows[2][0]["@request_id"];
 			res.json({ NEWID });
@@ -83,7 +105,10 @@ router.route('/request/getAllPublic')
     .get(function(req, res) {
         
         db_con.query('CALL sp_get_all_public_requests()', function(err,rows){
-			if (err) throw err;
+			if (err) {
+                console.log(err);
+                throw err;
+            }
 
 			res.json({ REQUESTS: rows[0] });
 		});
@@ -95,7 +120,10 @@ router.route('/request/getAllByUser')
     .post(function(req, res) {
 
         db_con.query('CALL sp_get_all_requests_for_user(?)', [req.body.userId], function(err,rows){
-			if (err) throw err;
+			if (err) {
+                console.log(err);
+                throw err;
+            }
 
 			res.json({ USERS_REQUESTS: rows[0] });
 		});
@@ -107,7 +135,10 @@ router.route('/request/delete')
     .post(function(req, res) {
 
         db_con.query('CALL sp_delete_request(?)', [req.body.requestId], function(err,rows){
-			if (err) throw err;
+			if (err) {
+                console.log(err);
+                throw err;
+            }
 
             res.end();
 		});
